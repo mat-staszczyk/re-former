025890b9bb2376d746abb397183b5fe8b85b98e0
sleaz0id <mat.staszczyk@gmail.com>
2016-12-11 17:58:03 +0100

    Passwords feature implemented

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test 'should be valid' do
+    @user.valid?
+  end
+    
 end
